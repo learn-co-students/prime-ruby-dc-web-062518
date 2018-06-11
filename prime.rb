f5faa9e8004596d5be5df73fdd5330f4b26e1f44
github username <github email address>
2018-06-10 22:06:50 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+  array = []
+  if number > 2
+    limit = number - 1
+    new_array = (2..limit).to_a
+    new_array.each do |no|
+      if number % no == 0
+        array << "true"
+      else
+        array << "false"
+      end
+    end
+    var = array.include?("true")
+    return not var
+  elsif number == 2
+    return true
+  elsif number <= 1
+    return false
+  end
+end
