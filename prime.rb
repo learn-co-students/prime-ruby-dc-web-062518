86e9d9e3b87d68c2b311a2064aef64301e53bd85
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-14 04:32:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+require 'pry'
+def prime?(num)
+  if num <= 1 
+    return false
+  elsif num == 2 || num == 3
+    return true
+  end
+  range = Array (2..(num-1))
+  #binding.pry
+  count = 0
+  while count < range.size 
+    if(num % range[count] == 0)
+      return false
+    end
+    count += 1
+  end
+  true
+end
